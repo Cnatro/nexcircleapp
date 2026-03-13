5f777ca1c8c6bd013559bef0e86d5bfed1336821
nhat722004@gmail.com <nhat722004@gmail.com>
2026-03-13 10:28:11 +0700

    main

---
+++ nexcircle/src/main/java/com/nexcircle/NexcircleApplication.java
@@ -0,0 +1,13 @@
+package com.nexcircle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexcircleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexcircleApplication.class, args);
+	}
+
+}
---
+++ nexcircle/src/test/java/com/nexcircle/NexcircleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexcircle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexcircleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
