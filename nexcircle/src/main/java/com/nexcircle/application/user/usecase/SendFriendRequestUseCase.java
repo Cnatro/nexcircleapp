9f30e192cd64f66a21d6899027f7a904e9f3447b
nhat722004@gmail.com <nhat722004@gmail.com>
2026-04-02 13:43:17 +0700

    update

--- nexcircle/src/main/java/com/nexcircle/application/user/usecase/SendFriendRequestUseCase.java
+++ nexcircle/src/main/java/com/nexcircle/application/user/usecase/SendFriendRequestUseCase.java
@@ -3,6 +3,7 @@ package com.nexcircle.application.user.usecase;
 import com.nexcircle.application.user.dto.FriendRequestDto;
 import com.nexcircle.application.user.mapper.FriendRequestMapper;
 import com.nexcircle.domain.user.repository.FriendRequestRepository;
+import com.nexcircle.domain.user.service.SecurityContextService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -16,8 +17,11 @@ import org.springframework.stereotype.Service;
 public class SendFriendRequestUseCase {
     FriendRequestRepository friendRequestRepository;
     FriendRequestMapper friendRequestMapper;
+    SecurityContextService securityContextService;
 
     public void createFrRequest(FriendRequestDto dto){
+
+        dto.setSenderId(this.securityContextService.getCurrentUserId());
         this.friendRequestRepository.save(this.friendRequestMapper.toEntity(dto));
     }
 }
